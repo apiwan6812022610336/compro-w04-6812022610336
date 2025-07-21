f97e50396f484fdc43b05479e583ad8991795899
Apiwat Datsing <s6812022610336@email.kmutnb.ac.th>
2025-07-21 13:44:08 +0700

    coding_woa-o1.c

--- Coding_wo4-01.c
+++ Coding_wo4-01.c
@@ -1,10 +1,9 @@
-#include<stdio.h>
+#include<stdio.h>  //#include keyword 
 
-int main() {
+int main() {  // int keyword // main identifier
     
-    printf("Hello\n");
-    printf("C Token!\n");
-    return 0;
-}
+    printf("Hello\n");  // printf identifier // Hello constant
+    printf("C Token!\n"); // printf identifier // C token constant
+    return 0; // return keyword // 0 constant
 
-
+} // } operator
