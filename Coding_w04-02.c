04826f6c2a2d4743e1fa18f04732ae3b2959cf70
Apiwan Datsing <s6812022610336@email.kmutnb.ac.th>
2025-07-22 23:10:51 +0700

    repo

---
+++ Coding_w04-02.c
@@ -0,0 +1,7 @@
+#include <stdio.h>  // เปลี่ยนจาก stdio.h เป็น <stdio.h> 
+
+int main() {        
+    int a = 10;  // เปลี่ยนจาก int a = 10 -> int a = 10; // int ชื่อตัวแปร a กำหนดค่าเริ่มต้นเป็น 10
+    printf("a = %d", a);  // เปลี่ยนจาก printf("a = %d", a) -> printf("a = %d", a); //
+    return 0;       // เปลี่ยนจาก return 0 -> return 0; //
+}
